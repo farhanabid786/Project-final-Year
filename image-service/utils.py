74be7751feeed8bcf6f34a24d9c7aaaf0965a7b5
FARHAN ABID <156011263+farhanabid786@users.noreply.github.com>
2026-04-22 17:19:22 +0530

    Refactor model download function to use requests
    Replaced gdown with requests for model downloading and added checks for existing model.

--- image-service/utils.py
+++ image-service/utils.py
@@ -1,13 +1,31 @@
+# import os
+# import gdown
+
+# def download_model(model_path, url):
+#     if not os.path.exists(model_path):
+#         print("⬇️ Downloading model from Google Drive...")
+
+#         gdown.download(url, model_path, quiet=False)
+
+#         if not os.path.exists(model_path):
+#             raise RuntimeError("❌ Model download failed!")
+
+#         print("✅ Model downloaded successfully")
+
+
 import os
-import gdown
+import requests
 
 def download_model(model_path, url):
-    if not os.path.exists(model_path):
-        print("⬇️ Downloading model from Google Drive...")
+    if os.path.exists(model_path):
+        return
 
-        gdown.download(url, model_path, quiet=False)
+    response = requests.get(url, stream=True)
 
-        if not os.path.exists(model_path):
-            raise RuntimeError("❌ Model download failed!")
+    if response.status_code != 200:
+        raise RuntimeError(f"Download failed: {response.status_code}")
 
-        print("✅ Model downloaded successfully")
+    with open(model_path, "wb") as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
