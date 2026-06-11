3dd8bd6f3a2778eca1e761f2c50898307c7d5bde
FARHAN ABID <156011263+farhanabid786@users.noreply.github.com>
2026-06-11 17:25:37 +0530

    Add files via upload

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,29 @@
+import express from "express";
+import cors from "cors";
+import passport from "passport";
+import "./config/passport.js";
 
+import authRoutes from "./modules/auth/auth.routes.js";
+import detectionRoutes from "./modules/detection/detection.routes.js";
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use(passport.initialize());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+// routes
+app.use("/api/auth", authRoutes);
+app.use("/api/detect", detectionRoutes);
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API working");
+});
+
+export default app;
